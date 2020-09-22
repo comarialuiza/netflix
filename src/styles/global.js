8f72b6f3d90cd9b84ed58dbd0d7c6e04e4343684
Maria Luiza <co.maria.luiza@gmail.com>
2020-09-22 14:55:35 -0300

    Creates try now input

--- src/styles/global.js
+++ src/styles/global.js
@@ -5,9 +5,12 @@ const GlobalStyle = createGlobalStyle`
         font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
-        background-color: #000;
         color: #333;
         font-size: 16px;
+    }
+
+    html, body {
+        background-color: #000;
     }
 `;
 
