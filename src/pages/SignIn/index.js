549e919d7a0f1fb1682f57e3546d81a21141a9c5
Maria Luiza <co.maria.luiza@gmail.com>
2020-09-27 22:45:16 -0300

    Add firebase authentication on sign up and sign in

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,18 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import HeaderContainer from './../../containers/HeaderContainer';
 import FooterContainer from './../../containers/FooterContainer';
 import Form from './../../components/Form';
 
+import { FirebaseContext } from './../../context/firebase';
+import * as ROUTES from './../../constants/routes'; 
+
 function SignIn() {
+    const { firebase } = useContext(FirebaseContext);
+    const history = useHistory();
+
     const [ emailAddress, setEmailAddress ] = useState('');
     const [ password, setPassword ] = useState('');
     const [ error, setError ] = useState('');
 
-    /* Check form input elements valid */
-
     const handleSignIn = e => {
         e.preventDefault();
+
+        firebase
+            .auth()
+            .signInWithEmailAndPassword(emailAddress, password)
+            .then(() => {
+                /* push to the browse page */
+                history.push(ROUTES.BROWSE);
+            })
+            .catch(error => {
+                setEmailAddress('');
+                setPassword('');
+                setError(error.message);
+            });
     }
 
     const isInvalid = password === '' || emailAddress === '';
@@ -40,7 +58,7 @@ function SignIn() {
                         <Form.Submit disabled={ isInvalid } type="submit">Sign In</Form.Submit>
                     </Form.Base>
 
-                    <Form.Text>New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link></Form.Text>
+                    <Form.Text>New to Netflix? <Form.Link to={ ROUTES.SIGN_UP }>Sign up now.</Form.Link></Form.Text>
                     <Form.TextSmall>This page is protected by Google reCAPTCHA to ensure you're not a bot. Learn more.</Form.TextSmall>
                 </Form>
             </HeaderContainer>
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,10 +1,86 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 
-// import { Container } from './styles';
+import HeaderContainer from './../../containers/HeaderContainer';
+import FooterContainer from './../../containers/FooterContainer';
+import Form from './../../components/Form';
+
+import { FirebaseContext } from './../../context/firebase';
+import * as ROUTES from './../../constants/routes'; 
 
 function SignUp() {
+    const history = useHistory();
+    const { firebase } = useContext(FirebaseContext);
+
+    const [ firstName, setFirstName ] = useState('');
+    const [ emailAddress, setEmailAddress ] = useState('');
+    const [ password, setPassword ] = useState('');
+    const [ error, setError ] = useState('');
+
+    const isInvalid = firstName === '' || password === '' || emailAddress === '';
+
+    const handleSignUp = event => {
+        event.preventDefault();
+
+        firebase
+            .auth()
+            .createUserWithEmailAndPassword(emailAddress, password)
+            .then(result => {
+                result.user
+                .updateProfile({
+                    displayName: firstName,
+                    photoURL: Math.floor(Math.random() * 5) + 1
+                })
+                .then(() => {
+                    history.push(ROUTES.BROWSE);
+                })
+            }).catch(error => {
+                setFirstName('');
+                setEmailAddress('');
+                setPassword('');
+                setError(error.message);
+            })
+    }
+
     return (
-        <h1>Sign in!</h1>
+        <>
+            <HeaderContainer>
+                <Form>
+                    <Form.Title>Sign Up</Form.Title>
+                    { error && <Form.Error>{ error }</Form.Error> }
+
+                    <Form.Base onSubmit={ handleSignUp } method="POST">
+                        <Form.Input 
+                            placeholder="First name"
+                            value={ firstName }
+                            onChange={({ target }) => setFirstName(target.value)}
+                        />
+
+                        <Form.Input 
+                            placeholder="Email address"
+                            value={ emailAddress }
+                            onChange={({ target }) => setEmailAddress(target.value)}
+                        />
+
+                        <Form.Input 
+                            placeholder="Password"
+                            value={ password }
+                            onChange={({ target }) => setPassword(target.value)}
+                            autoComplete="off"
+                            type="password"
+                        />
+
+                        <Form.Submit disabled={ isInvalid } type="submit">Sign Up</Form.Submit>
+                    </Form.Base>
+
+                    <Form.Text>Already a user? <Form.Link to={ ROUTES.SIGN_IN }>Sign in now.</Form.Link></Form.Text>
+                    <Form.TextSmall>
+                        This page is protected by Google reCAPTCHA to ensure you're not a bot. Learn more.
+                    </Form.TextSmall>
+                </Form>
+            </HeaderContainer>
+            <FooterContainer />
+        </>
     );
 }
 
